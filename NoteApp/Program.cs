55ff75142d732f814222dedf21c29102c64af3b9
Vlad <rutor787893@live.com>
2019-09-26 20:36:11 +0700

    Добавлен проект логики.

--- NoteApp/Program.cs
+++ NoteApp/Program.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using NoteApp;
 
-namespace NoteAppUI
+namespace NoteApp
 {
     static class Program
     {
@@ -17,7 +16,7 @@ namespace NoteAppUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new Form1());
         }
     }
 }
