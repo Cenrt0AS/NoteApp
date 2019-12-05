6e93ffd68e0d3378222365554945cd91759aef78
Vlad <rutor787893@live.com>
2019-12-05 15:39:10 +0700

    Попытка реализации 1го юнит теста.

--- NoteAppUI/NoteApp.UnitTests/ProjectTest.cs
+++ NoteAppUI/NoteApp.UnitTests/ProjectTest.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using NoteApp;
 
 namespace NoteApp.UnitTests
 {
+    [TestFixture] // Для того чтобы VS и adapter могли отличить простой класс от класса тестов.
     class ProjectTest
     {
+        /// <summary>
+        /// Unit тест для проверки геттера title в классе Note.
+        /// </summary>
+        /// 
+        public Note note; //?
+        public void TestTitleGet_CorrectValue()
+        {
+
+            var expected = "Заголовок";
+            var note = new Note();
+            note.Title = expected;
+            var actual = note.Title;
+
+            Assert.AreEqual(expected, actual, "Геттер Title возвращает неправильный заголовок."); 
+        }
+
     }
 }
