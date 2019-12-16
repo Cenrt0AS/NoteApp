68de25df9638634573b9e14bdb380bd0bc1b1c11
Vlad <rutor787893@live.com>
2019-12-16 20:52:30 +0700

    Рабочая сериализация и десериализация.

--- NoteAppUI/NoteApp.UnitTests/ProjectManagerTests.cs
+++ NoteAppUI/NoteApp.UnitTests/ProjectManagerTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using NoteApp;
 using NUnit.Framework;
 using System.IO;
+using System.Reflection;
 
 namespace NoteApp.UnitTests
 {
@@ -19,12 +20,23 @@ namespace NoteApp.UnitTests
         [Test(Description = "Тест сериализации.")]
         public void SerializeTest()
         {
-            string testFilePath = @"testSerialize.json";
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string testFilePath = $@"{path}\testSerialize.json";
             string expected = File.ReadAllText(testFilePath);
-            Note note = new Note("Тест", "Тестик", NoteCategory.Docs, new DateTime(2019, 12, 12), new DateTime(2019, 12, 12));
-            //string defaultPath = Environment.CurrentDirectory;
-            string filename = "testSerialize1.json";
-            _testproject.dictionary.Add(0, note);
+            Note note0 = new Note("Тест", "Тестик", NoteCategory.Docs, new DateTime(2019, 12, 12).Date, new DateTime(2019, 12, 12).Date);
+            Note note1 = new Note("Машина", "Купить машину", NoteCategory.Home, new DateTime(2019, 11, 11).Date, new DateTime(2019, 11, 11).Date);
+            Note note2 = new Note("Телефон", "Записать телефон", NoteCategory.HealthAndSport, new DateTime(2018, 12, 12).Date, new DateTime(2018, 12, 12).Date);
+            Note note3 = new Note("Встреча", "Встретиться с человеком", NoteCategory.People, new DateTime(2019, 12, 12).Date, new DateTime(2019, 12, 12).Date);
+              string filename = "testSerialize1.json";
+            //for (int i=0; i<=3; i++)
+            //  { 
+
+            //  _testproject.dictionary.Add(i, note[0]);
+            //  }
+            _testproject.dictionary.Add(0, note0);
+            _testproject.dictionary.Add(1, note1);
+            _testproject.dictionary.Add(2, note2);
+            _testproject.dictionary.Add(3, note3);
             ProjectManager.SaveToFile(_testproject, filename);
             string actual = File.ReadAllText(filename);
             Assert.AreEqual(expected, actual, "Файлы в сериализации различаются !");
@@ -33,24 +45,34 @@ namespace NoteApp.UnitTests
         [Test(Description = "Тест десериализации.")]
         public void DeserializeTest()
         {
-            Note note = new Note("Тест", "Тестик", NoteCategory.Docs, new DateTime(2019, 12, 12), new DateTime(2019, 12, 12));
-            _testproject2.dictionary.Add(0, note);
+            Note note0 = new Note("Тест", "Тестик", NoteCategory.Docs, new DateTime(2019, 12, 12).Date, new DateTime(2019, 12, 12).Date);
+            Note note1 = new Note("Машина", "Купить машину", NoteCategory.Home, new DateTime(2019, 11, 11).Date, new DateTime(2019, 11, 11).Date);
+            Note note2 = new Note("Телефон", "Записать телефон", NoteCategory.HealthAndSport, new DateTime(2018, 12, 12).Date, new DateTime(2018, 12, 12).Date);
+            Note note3 = new Note("Встреча", "Встретиться с человеком", NoteCategory.People, new DateTime(2019, 12, 12).Date, new DateTime(2019, 12, 12).Date);
+            _testproject2.dictionary.Add(0, note0);
+            _testproject2.dictionary.Add(1, note1);
+            _testproject2.dictionary.Add(2, note2);
+            _testproject2.dictionary.Add(3, note3);
             Project expected = _testproject2;
-            string filename = @"testSerialize.json";
-            Project preactual = ProjectManager.LoadFromFile(filename);
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string testFilePath = $@"{path}\testSerialize.json";
+            Project preactual = ProjectManager.LoadFromFile(testFilePath);
             Project actual = preactual;
-    
-            //TODO: выасыв
-            Assert.AreEqual(expected.dictionary[0].Title, actual.dictionary[0].Title, 
+            int i = 0;
+            foreach (KeyValuePair<int, Note> kvp in _testproject2.dictionary)
+            { 
+                Assert.AreEqual(expected.dictionary[i].Title, actual.dictionary[i].Title, 
                 "Значения в десериализации различаются !");
-            Assert.AreEqual(expected.dictionary[0].NoteText, actual.dictionary[0].NoteText,
+            Assert.AreEqual(expected.dictionary[i].NoteText, actual.dictionary[i].NoteText,
                 "Значения в десериализации различаются !");
-            Assert.AreEqual(expected.dictionary[0].Category, actual.dictionary[0].Category,
+            Assert.AreEqual(expected.dictionary[i].Category, actual.dictionary[i].Category,
                 "Значения в десериализации различаются !");
-            Assert.AreEqual(expected.dictionary[0].DateofCreation, actual.dictionary[0].DateofCreation,
+            Assert.AreEqual(expected.dictionary[i].DateofCreation, actual.dictionary[i].DateofCreation,
                 "Значения в десериализации различаются !");
-            Assert.AreEqual(expected.dictionary[0].LastmodDate, actual.dictionary[0].LastmodDate,
+            Assert.AreEqual(expected.dictionary[i].LastmodDate, actual.dictionary[i].LastmodDate,
                 "Значения в десериализации различаются !");
+                i++;
+            }
         }
     }
 }
