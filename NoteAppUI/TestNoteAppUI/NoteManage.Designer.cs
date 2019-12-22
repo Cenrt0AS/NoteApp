351185da3f861036380faaf747ddfa0954505944
Vlad <rutor787893@live.com>
2019-12-23 00:30:32 +0700

    Добавлена иконка в  окно редактирование.

--- NoteAppUI/TestNoteAppUI/NoteManage.Designer.cs
+++ NoteAppUI/TestNoteAppUI/NoteManage.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NoteManage));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -159,6 +160,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(410, 310);
             this.Name = "NoteManage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
